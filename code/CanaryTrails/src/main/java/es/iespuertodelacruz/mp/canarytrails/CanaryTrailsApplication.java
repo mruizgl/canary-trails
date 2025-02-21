fdfcdfd8796f10f269a8ba599c4d9358e4074e2b
PeterMartEsc <petermartesc@gmail.com>
2025-02-21 16:50:43 +0000

    feat(code): Se añade el proyecto de java

---
+++ code/CanaryTrails/src/main/java/es/iespuertodelacruz/mp/canarytrails/CanaryTrailsApplication.java
@@ -0,0 +1,13 @@
+package es.iespuertodelacruz.mp.canarytrails;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CanaryTrailsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CanaryTrailsApplication.class, args);
+	}
+
+}
---
+++ code/CanaryTrails/src/test/java/es/iespuertodelacruz/mp/canarytrails/CanaryTrailsApplicationTests.java
@@ -0,0 +1,13 @@
+package es.iespuertodelacruz.mp.canarytrails;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CanaryTrailsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
