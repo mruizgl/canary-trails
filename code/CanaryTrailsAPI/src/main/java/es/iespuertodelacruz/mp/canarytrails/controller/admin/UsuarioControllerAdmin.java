d3148cd93b2804c87216211ec230c5ae9612ffd1
Melissa Ruiz <125923092+mruizgl@users.noreply.github.com>
2025-05-15 18:22:58 +0200

    feat(usuarioController): se realiza el controller admin de usuario

--- code/CanaryTrailsAPI/src/main/java/es/iespuertodelacruz/mp/canarytrails/controller/admin/UsuarioControllerAdmin.java
+++ code/CanaryTrailsAPI/src/main/java/es/iespuertodelacruz/mp/canarytrails/controller/admin/UsuarioControllerAdmin.java
@@ -1,17 +1,20 @@
 package es.iespuertodelacruz.mp.canarytrails.controller.admin;
 
 import es.iespuertodelacruz.mp.canarytrails.dto.usuario.UsuarioEntradaCreateDto;
-import es.iespuertodelacruz.mp.canarytrails.dto.usuario.auth.UsuarioRegisterDto;
+import es.iespuertodelacruz.mp.canarytrails.dto.usuario.UsuarioEntradaUpdateDto;
+import es.iespuertodelacruz.mp.canarytrails.dto.usuario.UsuarioSalidaDto;
 import es.iespuertodelacruz.mp.canarytrails.entities.Usuario;
+import es.iespuertodelacruz.mp.canarytrails.mapper.UsuarioMapper;
 import es.iespuertodelacruz.mp.canarytrails.service.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 /**
- * Controller para los endpoints de Usuario.
- * /api/v1/admin/usuarios indica que es la versión para administradores y el endpoint de usuarios
- * En caso de cambiar la versión de la api, cambiaria a v2
+ * Controller para los endpoints de Usuario desde el administrador.
  */
 @RestController
 @RequestMapping("/api/v1/admin/usuarios")
@@ -21,44 +24,83 @@ public class UsuarioControllerAdmin {
     @Autowired
     UsuarioService usuarioService;
 
-    @GetMapping
-    public ResponseEntity<?> findAllUsuarios(){
+    @Autowired
+    UsuarioMapper usuarioMapper;
 
-        //TODO: pasar por el dto, comentar como en los otros controller
-        return ResponseEntity.ok(usuarioService.findAll());
+    /**
+     * Endpoint para obtener todos los usuarios
+     */
+    @GetMapping
+    public ResponseEntity<?> findAllUsuarios() {
+        List<Usuario> usuarios = usuarioService.findAll();
+        List<UsuarioSalidaDto> usuariosDto = usuarios.stream()
+                .map(usuarioMapper::toDto)
+                .collect(Collectors.toList());
+        return ResponseEntity.ok(usuariosDto);
     }
 
+    /**
+     * Endpoint para obtener un usuario a partir de su id
+     * @param id del usuario que se quiere obtener
+     * @return el usuario cuyo id coincide con el introducido
+     */
     @GetMapping("/{id}")
-    public ResponseEntity<?> findUsuarioById(@PathVariable Integer id){
+    public ResponseEntity<?> findUsuarioById(@PathVariable Integer id) {
 
         Usuario usuario = usuarioService.findById(id);
 
-        if(usuario == null){
+        if (usuario == null) {
             return ResponseEntity.notFound().build();
         }
+        UsuarioSalidaDto dto = usuarioMapper.toDto(usuario);
 
-        //TODO: devolver como dto
-        return ResponseEntity.ok(usuario);
+        return ResponseEntity.ok(dto);
     }
 
+    /**
+     * Endpoint para añadir un usuario a la bbdd
+     * @param dto con los datos que se deben introducir para crear el usuario
+     * @return un JSON del usuario guardado
+     */
     @PostMapping("/add")
-    public ResponseEntity<?> createUsuario(@RequestBody UsuarioEntradaCreateDto dto){
+    public ResponseEntity<?> createUsuario(@RequestBody UsuarioEntradaCreateDto dto) {
 
+        Usuario usuario = usuarioMapper.toEntityCreate(dto);
 
-        //TODO: mapear ode dto create a entidad, hacer el save
-
-        Usuario usuario = new Usuario();
-        usuario.setNombre(dto.nombre());
-        usuario.setApellidos(dto.apellidos());
-        usuario.setCorreo(dto.correo());
-        usuario.setPassword(dto.password());
-
-        //TODO: mapeado a dto salida
-        return ResponseEntity.ok(usuarioService.save(usuario));
+        try {
+            usuario = usuarioService.save(usuario);
+        } catch (RuntimeException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+        return ResponseEntity.ok(usuarioMapper.toDto(usuario));
     }
 
-    //TODO: UPDATE mapear de Entrada update a dto, hacer el update, devolver un response entity con el usuarioService.update(usuario)
+    /**
+     * Endpoint que actualiza un usuario en la bbdd
+     * @param dto con el objeto a actualizar, tiene que contener la id del objeto
+     * @return true si se ha actualizado y false si no
+     */
+    @PutMapping("/update")
+    public ResponseEntity<?> updateUsuario(@RequestBody UsuarioEntradaUpdateDto dto) {
 
-    //TODO: DELETE tal cual los otros delete, pero haciendo un catch del runtime exception (ejemplo sencillo en controller de zona)
+        Usuario usuario = usuarioMapper.toEntityUpdate(dto);
 
+        boolean actualizado;
+        try {
+            actualizado = usuarioService.update(usuario);
+        } catch (RuntimeException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+        return ResponseEntity.ok(actualizado);
+    }
+
+    /**
+     * Borra un usuario a partir de la id
+     * @param id del usuario a borrar
+     * @return true si se ha borrado, false si no
+     */
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<?> deleteUsuario(@PathVariable("id") int id) {
+       return ResponseEntity.ok(usuarioService.deleteById(id));
+    }
 }
