b3db14caaf8c9ca1ab695fd63c49ba6d5e03b41e
PeterMartEsc <petermartesc@gmail.com>
2025-05-15 15:47:57 +0200

    feat

--- code/CanaryTrailsAPI/src/main/java/es/iespuertodelacruz/mp/canarytrails/controller/admin/UsuarioControllerAdmin.java
+++ code/CanaryTrailsAPI/src/main/java/es/iespuertodelacruz/mp/canarytrails/controller/admin/UsuarioControllerAdmin.java
@@ -2,11 +2,15 @@ package es.iespuertodelacruz.mp.canarytrails.controller.admin;
 
 import es.iespuertodelacruz.mp.canarytrails.dto.usuario.UsuarioEntradaCreateDto;
 import es.iespuertodelacruz.mp.canarytrails.dto.usuario.auth.UsuarioRegisterDto;
+import es.iespuertodelacruz.mp.canarytrails.entities.Fauna;
 import es.iespuertodelacruz.mp.canarytrails.entities.Usuario;
+import es.iespuertodelacruz.mp.canarytrails.service.FotoManagementService;
 import es.iespuertodelacruz.mp.canarytrails.service.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 /**
  * Controller para los endpoints de Usuario.
@@ -20,6 +24,9 @@ public class UsuarioControllerAdmin {
 
     @Autowired
     UsuarioService usuarioService;
+
+    @Autowired
+    FotoManagementService fotoManagementService;
 
     @GetMapping
     public ResponseEntity<?> findAllUsuarios(){
@@ -60,5 +67,28 @@ public class UsuarioControllerAdmin {
     //TODO: UPDATE mapear de Entrada update a dto, hacer el update, devolver un response entity con el usuarioService.update(usuario)
 
     //TODO: DELETE tal cual los otros delete, pero haciendo un catch del runtime exception (ejemplo sencillo en controller de zona)
+
+    @PostMapping(value = "/upload/{id}", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<?> uploadFile(@RequestParam("id") Integer id, @RequestParam("file") MultipartFile file) {
+
+        String mensaje = "";
+        String categoria = "usuario";
+
+        try {
+            String namefile = fotoManagementService.save(file, categoria);
+            mensaje = "" + namefile;
+
+            Usuario usuario = usuarioService.findById(id);
+
+            usuario.setFoto(namefile);
+            usuarioService.update(usuario);
+
+            return ResponseEntity.ok(mensaje);
+        } catch (Exception e) {
+            mensaje = "No se pudo subir el archivo: " + file.getOriginalFilename()
+                    + ". Error: " + e.getMessage();
+            return ResponseEntity.badRequest().body(mensaje);
+        }
+    }
 
 }
