e60293590585b6f70a84770f5c41075d07116367
PeterMartEsc <petermartesc@gmail.com>
2025-05-17 18:18:54 +0200

    refactor(): cambios menores

--- code/AppCanaryTrails/src/screens/auth/Register.tsx
+++ code/AppCanaryTrails/src/screens/auth/Register.tsx
@@ -18,16 +18,23 @@ const Register = ({navigation, route}:PropsHome) => {
     const [password, setpassword] = useState("")
     const [correo, setcorreo] = useState("")
 
-    async function login() {
+    async function register() {
         
+        const datosRegister ={
+            nombre: nombre, 
+            correo: correo,
+            password: password
+        }
+
         try {
-            const response = await axios.post(`'http://localhost:8080/api/v1/register'`, {
-                nombre, password, correo
+            const response = await axios.post(`http://10.0.2.2:8080/api/v1/auth/register`, {
+                datosRegister
             });
             
             if (response.status === 200) {
                 saveToken(response.data);
             }
+
         } catch (error) {
             console.log("An error has occurred aqui" +error.message);
 
@@ -37,18 +44,18 @@ const Register = ({navigation, route}:PropsHome) => {
     <>
       <View>
             <TextInput placeholder='nombre' onChangeText={(texto) => setnombre(texto)} />
-            <TextInput placeholder='password' secureTextEntry={true} onChangeText={(texto) => setpassword(texto)} />
             <TextInput placeholder='correo' onChangeText={(texto) => setcorreo(texto)} />
+            <TextInput placeholder='password' secureTextEntry={true} onChangeText={(texto) => setpassword(texto)} />
 
             <View style={{borderWidth: 2, borderColor: 'blue' , margin: 10, padding: 2, alignItems: 'center'}}>
-                <TouchableHighlight onPress={login}>
+                <TouchableHighlight onPress={register}>
                     <Text>Register</Text>
                 </TouchableHighlight>
             </View>
 
             <View>
                 <TouchableHighlight onPress={()=> navigation.navigate('Login')}>
-                    <Text style={{color: 'blue', textAlign: 'center', margin: 20}}>No tengo cuenta</Text>
+                    <Text style={{color: 'blue', textAlign: 'center', margin: 20}}>Ya tengo cuenta</Text>
                 </TouchableHighlight>
             </View>
         </View>
