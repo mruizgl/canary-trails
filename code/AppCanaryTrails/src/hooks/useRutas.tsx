fc2037a89dd4ba4ad2387ac05d1f8251755453a2
PeterMartEsc <petermartesc@gmail.com>
2025-05-21 08:53:48 +0100

    feat(hooks): Se añaden dos hooks para usuarios y rutas

---
+++ code/AppCanaryTrails/src/hooks/useRutas.tsx
@@ -0,0 +1,101 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React, { useEffect, useState } from 'react'
+import { RutaType, tokenPlayload } from '../globals/Types'
+import axios from 'axios'
+import { useAppContext } from '../context/AppContext'
+import { useJwt } from 'react-jwt'
+
+type Props = {}
+
+const useRutas = (props: Props) => {
+
+    const context = useAppContext();
+    const { decodedToken } = useJwt<tokenPlayload>(context.token);
+
+    const [allRutas, setAllRutas] = useState<Array<RutaType>>([])
+    const [rutasRandom, setRutasRandom] = useState<Array<RutaType>>([])
+    const [rutasPopulares, setRutasPopulares] = useState<Array<RutaType>>([])
+
+    useEffect(() => {
+        //Setea allRutas y RutasRandom
+        getRutasPopulares();
+        getAllRutas();
+    }, [])
+
+    async function getAllRutas(){
+        
+        let rutasAux : Array<RutaType> = [];
+
+        try{
+
+            const response = await axios.get(`http://10.0.2.2:8080/api/v2/rutas`,
+            {
+                headers: {
+                'Authorization': `Bearer ${context.token}`, // Token JWT
+                'Content-Type': 'application/json', // Tipo de contenido
+                }
+            }
+            );
+
+            rutasAux = response.data;
+
+        } catch (error) {
+            console.log("An error has occurred aqui" +error.message);
+        }
+
+        
+        rutasAux = rutasAux.filter((ruta: RutaType) => ruta.aprobada === true); //Filtrado si la ruta esá aprobada se muestra
+        setAllRutas(rutasAux);
+        console.log("All rutas seteadas");
+
+
+        let rutasRandomAux : Array<RutaType>= [];
+
+        if (rutasAux.length >= 5) {
+            const rutasMezcladas = rutasAux.sort(() => 0.5 - Math.random()); //Genera un valor random entre -0.5 y 0.5 
+            rutasRandomAux = rutasMezcladas.slice(0, 5); //Mete los 5 primeros elementos en el array (del 0 al 4)
+        } else {
+            rutasRandomAux = rutasAux;
+        }
+
+        setRutasRandom(rutasRandomAux);
+        console.log("Rutas random seteadas");
+    }
+
+    async function getRutasPopulares(){
+
+        let rutasAux : Array<RutaType> = [];
+      
+        try{
+  
+            const response = await axios.get(`http://10.0.2.2:8080/api/v2/rutas_favoritas/populares`,
+                {
+                    headers: {
+                        'Authorization': `Bearer ${context.token}`, // Token JWT
+                        'Content-Type': 'application/json', // Tipo de contenido
+                    }
+                }
+            );
+  
+          rutasAux = response.data;
+  
+        } catch (error) {
+          console.log("An error has occurred aqui" +error.message);
+        }
+  
+        setRutasPopulares(rutasAux);
+    }
+
+    
+    
+
+  return {
+    allRutas,
+    rutasPopulares,
+    rutasRandom,
+  }
+}
+
+export default useRutas
+
+const styles = StyleSheet.create({})
---
+++ code/AppCanaryTrails/src/hooks/useUser.tsx
@@ -0,0 +1,120 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React, { useEffect, useState } from 'react'
+import { useAppContext } from '../context/AppContext';
+import { useJwt } from 'react-jwt';
+import { RutaType, tokenPlayload, Usuario } from '../globals/Types';
+import axios from 'axios';
+
+type Props = {}
+
+const useUser = (props: Props) => {
+
+    const context = useAppContext();
+    const { decodedToken } = useJwt<tokenPlayload>(context.token);
+
+    const [usuarioLogueado, setUsuarioLogueado] = useState<Usuario>()
+    const [rutasFavoritasByUsuario, setRutasFavoritasByUsuario] = useState<Array<RutaType>>([])
+
+    useEffect(() => {
+
+        obtenerUsuario();
+
+    }, [decodedToken])
+
+    useEffect(() => {
+
+        obtenerRutasFavoritasByUsuario();
+      
+    }, [usuarioLogueado])
+    
+
+    async function obtenerUsuario(){
+
+        if(decodedToken == null){
+            return;
+        }
+
+        const nombre = decodedToken?.sub;
+
+        try{
+
+            const response = await axios.get(`http://10.0.2.2:8080/api/v2/usuarios/nombre/${nombre}`,
+
+                {
+                    headers: {
+                        'Authorization': `Bearer ${context.token}`, // Token JWT
+                        'Content-Type': 'application/json', // Tipo de contenido
+                    }
+                }
+            );
+
+            const usuario : Usuario = {
+                id: response.data.id,
+                nombre: response.data.nombre,
+                correo: response.data.correo,
+                password: response.data.password,
+                foto: response.data.foto,
+                rol: response.data.rol,
+                fechaCreacion: response.data.fechaCreacion,
+                faunas: response.data.faunas,
+                floras: response.data.floras,
+                rutas: response.data.rutas,
+                comentarios: response.data.comentarios,
+                rutasFavoritas: response.data.rutasFavoritas
+            }
+
+            setUsuarioLogueado(usuario);
+            console.log("Setteado el usuario logueado");
+
+        } catch (error) {
+          console.log("An error has occurred aqui" +error.message);
+        }
+
+    }
+
+    async function obtenerRutasFavoritasByUsuario(){
+
+        if(usuarioLogueado == null){
+            return;
+        }
+
+        let rutasAux : Array<RutaType> = [];
+
+        try{
+
+            const response = await axios.get(`http://10.0.2.2:8080/api/v2/rutas_favoritas/${usuarioLogueado.id}`,
+
+                {
+                    headers: {
+                        'Authorization': `Bearer ${context.token}`, // Token JWT
+                        'Content-Type': 'application/json', // Tipo de contenido
+                    }
+                }
+            );
+
+            rutasAux = response.data;
+            
+            setRutasFavoritasByUsuario(rutasAux);
+            console.log("Setteadas rutas favoritas del usuario logueado");
+
+        } catch (error) {
+          console.log("An error has occurred aqui" +error.message);
+        }
+    }
+
+    function esFavoritaDeUser(idRuta : number) : boolean{
+
+        //Some devuelve true o false directamente y se detiene cuando encuentra la coincidencia. Más optimo que el filter
+        return rutasFavoritasByUsuario.some((ruta: RutaType) => ruta.id === idRuta);
+    }
+
+    return {
+        usuarioLogueado,
+        rutasFavoritasByUsuario,
+        esFavoritaDeUser,
+    }
+}
+
+export default useUser
+
+const styles = StyleSheet.create({})
